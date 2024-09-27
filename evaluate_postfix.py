a4b5647d8f39b505eaf72af38a65d83ded50f7a7
JayaChaitanya10 <jayachaitanya2004@gmail.com>
2024-09-27 17:59:09 +0530

    Add files via upload

---
+++ evaluate_postfix.py
@@ -0,0 +1,17 @@
+def evaluate_postfix(expression):
+    stack = []
+    for token in expression.split():
+        if token.lstrip('-').isdigit():
+            stack.append(int(token))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            if token == '+':
+                stack.append(a + b)
+            elif token == '-':
+                stack.append(a - b)
+            elif token == '*':
+                stack.append(a * b)
+            elif token == '/':
+                stack.append(int(a / b))
+    return stack[0]
